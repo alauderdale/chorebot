a13aa286fff71ced2339a2461f7f0bbe5f5cb46e
Alex Lauderdale <alauderdale@mac.com>
2012-07-06 17:17:18 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chorebot::Application.config.secret_token = '760de2a7a931f8ec741b87c5a6b379c9a2d8fa6bb973643b1b0e9301ae6741ffbf4e77f3f813adf31a23312c41cda8d2c8494b1c89e182a6c702381468a4bb72'
